c0bc207a8d7b6589e87c45a3763b93f8f6e514a6
chaNcharge <chancharge@gmail.com>
2018-04-20 09:29:07 -0700

    Initial Commit

---
+++ Secret Message Challenge/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Secret Message Challenge
+//
+//  Created by Ethan Cha on 4/20/18.
+//  Copyright © 2018 Ethan Cha. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
