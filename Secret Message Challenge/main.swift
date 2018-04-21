404ab0abeb581cda29ed2df046c59dbc08be8cee
chaNcharge <chancharge@gmail.com>
2018-04-20 21:23:56 -0700

    Finish secret message challenge, prepare to fix bugs

--- Secret Message Challenge/main.swift
+++ Secret Message Challenge/main.swift
@@ -17,7 +17,31 @@ if choice == "encrypt" {
     print("Please enter a message to encrypt")
 } else if choice == "decrypt" {
     print("Please enter a message to decrypt")
+} else {
+    enum optionError: Error {
+        case ValueError(String)
+    }
+    throw optionError.ValueError("Invalid option")
 }
 
 let message = readLine()!
-print(message)
+print("Encryption key")
+let key = Int(readLine()!)!
+for character in message {
+    if alphabet.contains(character) {
+        let range: Range<String.Index> = alphabet.range(of: String(character))!
+        let position: Int = alphabet.distance(from: alphabet.startIndex, to: range.lowerBound)
+        var newPosition = 0
+        if choice == "encrypt" {
+            newPosition = (position + key) % 26
+        } else if choice == "decrypt" {
+            newPosition = (position - key) % 26
+        }
+        let index = alphabet.index(alphabet.startIndex, offsetBy: newPosition)
+        let newCharacter = alphabet[index]
+        newMessage += String(newCharacter)
+    } else {
+        newMessage += String(character)
+    }
+}
+print(newMessage)
